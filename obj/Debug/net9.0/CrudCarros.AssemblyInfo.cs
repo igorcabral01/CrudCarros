dc3d131dce87559ab5f6795b47037f29f62acad7
Igor Eduardo <153324985+igorcabral01@users.noreply.github.com>
2025-04-30 08:26:29 -0300

    feature

--- obj/Debug/net9.0/CrudCarros.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrudCarros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudCarros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30fb47910e40d3e104111151a9cdd340ab5cfa3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120fb32020deaed92865ce4d249a9c2973b5cc90")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudCarros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudCarros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
